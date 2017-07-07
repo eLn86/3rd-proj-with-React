a20397f35ef552c1dcb9875911cf76d5a0c77dd1
Alex Min <m98stone@gmail.com>
2017-07-07 16:23:25 +0800

    put back view and public folder for test.

--- backend/app.js
+++ backend/app.js
@@ -131,7 +131,7 @@ app.use((err, req, res, next) => {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  //res.render('error');
 });
 
 // Handle uncaughtException
