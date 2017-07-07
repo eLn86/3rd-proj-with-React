c0b805994bee10d8891a143d658f327fdc52f3cd
HanXiangC <Choongh@bc.edu>
2017-07-07 12:54:12 +0800

    Added: Prefences, Home, StartBtn

---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,61 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+// Children Components
+import Footer from '../Partials/Footer/Footer';
+import Header from '../Partials/Header/Header';
+
+import PreferenceBar from '../Preferences/PreferenceBar/PreferenceBar';
+import PreferenceTrending from '../Preferences/PreferenceTrending/PreferenceTrending';
+import StartBtn from './StartBtn/StartBtn';
+
+import './Home.css';
+
+
+/**
+ * Login
+ */
+export class Home extends Component { // eslint-disable-line react/prefer-stateless-function
+
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+
+    return (
+      <div className="container-fluid login">
+
+        <div className= "col-md-12 preferenceContainer">
+          <div className= "col-md-12 preferenceContainer">
+            <PreferenceBar/>
+          </div>
+          <div className= "col-md-12 preferenceContainer">
+            <PreferenceTrending/>
+          </div>
+        </div>
+
+        <div className= "col-md-12 startBtnContainer">
+          <StartBtn/>
+        </div>
+
+        <Footer/>
+
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // nothing to see here...
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
