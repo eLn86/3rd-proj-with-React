07d1b98b4dc07bcc43ba841f69e386b7ecad0437
Alex Min <m98stone@gmail.com>
2017-07-08 14:05:07 +0800

    Facebook Oauth hotfix, socket connected.

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -10,6 +10,24 @@ import {
 import Login from '../Login/Login';
 import Home from '../Home/Home'
 
+import io from 'socket.io-client';
+
+const socket = io('/');
+
+socket.on('show client', (id) => {
+  console.log('This is from server, socket is connected well.');
+  console.log(id);
+})
+
+const msg = 'hello world';
+setInterval(() => {
+  socket.emit('show connection', msg);
+}, 10000);
+
+socket.on('update userList', (arr) => {
+  console.log(arr);
+})
+
 class App extends Component {
   render() {
     return (
