dbae7cd6162ba8c77ea9f195bee3728b786e9011
Robyn Kwok <robyn.kwok@duke.edu>
2017-07-10 15:10:39 +0800

    changes

--- frontend/src/actions/preferenceActions.js
+++ frontend/src/actions/preferenceActions.js
@@ -1,19 +1,19 @@
-// import axios from 'axios';
-//
-// export const addPreferenceToStore = (preference) => {
-//   return {
-//     type: 'ADD_PREFERENCE',
-//     preference
-//   }
-// }
-//
-// export const deletePreferenceFromStore = (preference) => {
-//   return {
-//     type: 'DELETE_PREFERENCE',
-//     preference
-//   }
-// }
-//
+import axios from 'axios';
+
+export const addPreference = (preference) => {
+  return {
+    type: 'ADD_PREFERENCE',
+    preference
+  }
+}
+
+export const deletePreference = (preference) => {
+  return {
+    type: 'DELETE_PREFERENCE',
+    preference
+  }
+}
+
 // export const addPreference = () => {
 //   return (dispatch) => {
 //     dispatch(addPreferenceToStore(preference));
