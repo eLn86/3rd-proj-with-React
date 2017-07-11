2ef1c0fb2dc89cd5a0cd761bec97e1eab4161cfc
Robyn Kwok <robyn.kwok@duke.edu>
2017-07-11 11:02:35 +0800

    removed video test

--- frontend/src/components/Room/Room.js
+++ frontend/src/components/Room/Room.js
@@ -32,38 +32,38 @@ export class Room extends Component { // eslint-disable-line react/prefer-statel
     socket.emit('join room channel', roomName);
 
 
-  const video = document.querySelector('video');
-  const toggle = document.querySelector('.togglebtn');
+//   const video = document.querySelector('video');
+//   const toggle = document.querySelector('.togglebtn');
 
-  /*
-  * CONSTRAINTS: specify type of media to request
-  * properties can either be boolean or be objects for more specificity
-  * e.g. mandatory or optional, width, height, quality etc.
-  */
-  const constraints = {
-    audio: false,
-    video: {
-        frameRate: {
-          max: 10
-        }
-    }
-  }
+//   /*
+//   * CONSTRAINTS: specify type of media to request
+//   * properties can either be boolean or be objects for more specificity
+//   * e.g. mandatory or optional, width, height, quality etc.
+//   */
+//   const constraints = {
+//     audio: false,
+//     video: {
+//         frameRate: {
+//           max: 10
+//         }
+//     }
+//   }
 
-  // success: if video received, append to html element
-  this.handleSuccess = (stream) => {
-    window.stream = stream;
-    video.srcObject = stream;
-  }
+//   // success: if video received, append to html element
+//   this.handleSuccess = (stream) => {
+//     window.stream = stream;
+//     video.srcObject = stream;
+//   }
 
-  // failure: if video failed, log error
-  this.handleError = (error) => {
-    throw error.name;
-  }
+//   // failure: if video failed, log error
+//   this.handleError = (error) => {
+//     throw error.name;
+//   }
 
-  // Get User Media
-  navigator.mediaDevices.getUserMedia(constraints)
-  .then(this.handleSuccess)
-  .catch(this.handleError);
+//   // Get User Media
+//   navigator.mediaDevices.getUserMedia(constraints)
+//   .then(this.handleSuccess)
+//   .catch(this.handleError);
 
   // // TOGGLE play and pause
   // toggle.addEventListener('click', () => {
