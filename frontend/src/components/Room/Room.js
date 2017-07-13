46873f9b812f9b5f6c7b02077c899b2b4fba6d69
Alex Min <m98stone@gmail.com>
2017-07-13 12:30:30 +0800

    Flex login page, login info in header

--- frontend/src/components/Room/Room.js
+++ frontend/src/components/Room/Room.js
@@ -125,7 +125,7 @@ export class Room extends Component { // eslint-disable-line react/prefer-statel
   render() {
 
     return (
-      <div className="room mainBox">
+      <div className="room">
         {this.props.isFetching[0] ? (
           <div className="fetcherWrapper">
             {/* This is Wait! Screen for logout*/}
@@ -133,7 +133,7 @@ export class Room extends Component { // eslint-disable-line react/prefer-statel
             <div className="fetching">Logging you out...</div>
           </div>
         ) : (
-          <div>
+          <div className="mainBox">
           {/* This is room screen */}
           <Header/>
           <div className="row roomBody">
@@ -204,7 +204,7 @@ export class Room extends Component { // eslint-disable-line react/prefer-statel
 // grab current preferences from redux state
 const mapStateToProps = (state) => {
     return {
-      users: state.users,
+      user: state.user,
       preferences: state.preferences,
       roomName: state.rooms,
       isFetching: state.isFetching
